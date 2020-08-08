409ab1558c1f2e1c2c52cfa95f370fcd2a4b704c
Tyler <tyler.cook@incontextsolutions.com>
2020-08-07 21:48:47 -0400

    - Added package.json - Removed files unnecessary for a Unity package - Added meta files - Added asmdef

---
+++ Samples/Basic/CsvTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using UnityEngine;
+
+public class CsvTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+	    using (var reader = new StreamReader("path\\to\\file.csv"))
+	    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+	    {
+		    var records = csv.GetRecords<string>();
+	    }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Unity Build.bat
+++
@@ -1,5 +0,0 @@
-rmdir /S /Q unity\Assets\CsvHelper
-
-dotnet build src\CsvHelper\CsvHelper.csproj -c release -o ..\..\unity\Assets\CsvHelper
-
-pause
